8b3e5633b1d0b0b653ba39c02a6a45b486cd5561
Ali Chueib <alichueib@gmail.com>
2026-03-15 16:24:42 +0100

    Added test scenario2: Testing searchthreshold behavior

--- Bench/test1.vhd
+++ Bench/test1.vhd
@@ -37,29 +37,90 @@ begin
     -- successfull retrieval of food (ideal scenario/ Just go to the next state until u reach resting again):
     process
     begin
+        -- wait for 50 ns; -- this includes 10 for ensure changing the state from idle to Resting + 40 to finish resting
+
+        -- --now here we should be at 'randomwalk' state
+        -- findfood1 <= '1';
+        -- wait for 10 ns;
+        -- findfood1 <= '0';
+
+        -- --here at 'movetofood':
+        -- closetofood1 <= '1';
+        -- wait for 10 ns;
+        -- closetofood1 <= '0';
+
+        -- --here at grabfood:
+        -- success1 <= '1';
+        -- wait for 10 ns;
+        -- success1 <= '0';
+
+        -- --here at movetohome:
+        -- athome1 <= '1';
+        -- wait for 10 ns;
+        -- athome1 <= '0';
+
+        -- --here at deposit:
+        -- success1 <= '1';
+        -- wait for 10 ns;
+        -- success1 <= '0';
+
+        -- --Here we should be back to resting:
+
+
+        --senario 2: "Lets test rest threshold"
+        
+        --testing1: "Reach threshold from randomwalk state"
         wait for 50 ns; -- this includes 10 for ensure changing the state from idle to Resting + 40 to finish resting
 
-        --now here we should be at 'randomwalk' state
-        findfood1 <= '1';
-        wait for 10 ns;
-
-        --here at 'movetofood':
-        closetofood1 <= '1';
-        wait for 10 ns;
+        --now here we should be at 'randomwalk' state, so we will force the 10 cycles of search counters to finish, by waiting 100 ns,
+        wait for 100 ns;
 
 
-        --here at grabfood:
-        success1 <= '1';
-        wait for 10 ns;
-
-        --here at movetohome:
+        --now we should be at homing (as we reached thr)
         athome1 <= '1';
         wait for 10 ns;
+        athome1 <= '0';
 
-        --here at deposit:
-        success1 <= '1';
+        
+
+        --testing2: "Reach threshold from movetofood state"
+        --now I should be in resting again:
+        wait for 40 ns; 
+
+        --now at randomwalk:
+        findfood1 <= '1';
+        wait for 10 ns; 
+        findfood1 <= '0';
+
+        --now at movetofood
+        wait for 90 ns; --waiting for more 9 cycles (to reach searchthr)
+
+        --now at homing
+        athome1 <= '1';
         wait for 10 ns;
-        success1 <= '0';
+        athome1 <= '0';
+
+        --testing2: "Reach threshold from scanarena state"
+        --now I should be in resting again:
+        wait for 40 ns; 
+
+        --now at randomwalk:
+        findfood1 <= '1';
+        wait for 10 ns; 
+        findfood1 <= '0';
+
+        --now at movefood:
+        lostfood1 <= '1';
+        wait for 10 ns;
+        lostfood1 <= '0';
+
+        --now at scanarena
+        wait for 80 ns; --more 8 cycles to reach thr
+
+        --now at resting again...
+
+
+        --scenario three: "??"
 
     end process;
 
